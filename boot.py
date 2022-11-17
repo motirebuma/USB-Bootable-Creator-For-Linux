56c0d8f9575feb28d717b1b63d4af6419bfbc9fa
killo <motirebuma7@gmail.com>
2022-11-17 11:59:11 -0500

    banner

--- boot.py
+++ boot.py
@@ -3,15 +3,16 @@ from banner import banner
 from colorama import Fore
 
 
-banner.banner()
+banner.banner1()
 
 def boot():    
     try:
         os.system("sudo fdisk -l")
-        path = input(Fore.BLUE + "\nEnter path of OS: ")
+        path = input(Fore.BLUE + "\n\nEnter path of OS: ")
         device = input(Fore.BLUE +"Enter device name: [/dev/sdXX]: ")
+        os.system("clear")
         os.system("sudo dd if={} of={} bs=4M status=progress".format(path, device))
-
+        banner.banner2()
     except:
         print(Fore.RED + "\nSome thing went wrong!..")
         boot();
